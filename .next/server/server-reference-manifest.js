e5e8acfa8b627227b5564cee66397ea7c66a931e
tatesahil20-spec <tatesahil20@gmail.com>
2026-02-19 21:16:25 +0530

    update

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"SSmfhFqaKCQ2+Ka7K1zFA8LEubzvtJLdTRxHVVzAYYQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
